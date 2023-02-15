3139056c94e1b883d4123b7b370181f95fd520f9
Mac <sophanpa@gmail.com>
2023-02-15 22:45:09 +0100

    Add employee, nav & work field backgrounds

--- src/index.js
+++ src/index.js
@@ -23,6 +23,28 @@ function render_main_page() {
   const background = document.createElement('div');
   background.classList.add('background');
   body.append(background);
+
+  // create employee list
+  const employee_list_background = document.createElement('div');
+  employee_list_background.classList.add('employee_list_background');
+  background.append(employee_list_background);
+
+  // create employee searchbar
+  const employee_searchbar = document.createElement('input');
+  employee_searchbar.setAttribute('id', 'employee_searchbar');
+  employee_searchbar.type = 'text';
+  employee_searchbar.placeholder = 'search employee';
+  employee_list_background.append(employee_searchbar);
+
+  // create nav
+  const nav_background = document.createElement('div');
+  nav_background.classList.add('nav_background');
+  background.append(nav_background);
+
+  // create work field
+  const work_field_background = document.createElement('div');
+  work_field_background.classList.add('work_field_background');
+  background.append(work_field_background);
 }
 
 // Controller
